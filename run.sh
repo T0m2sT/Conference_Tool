6c6ccb10831836f613b7f76d8a1510720aba73d1
Pinho <rafaelpinhotsilva@gmail.com>
2026-03-28 16:43:30 +0000

    Changes to run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
 #!/bin/bash 
-mkdir code/cmake-build && cd code/cmake-build && cmake .. && make && cd ../.. && ./code/cmake-build/da_tool
+
+mkdir -p code/cmake-build &&
+cd code/cmake-build &&
+cmake .. &&
+make &&
+cd ../.. &&
+./code/cmake-build/da_tool
