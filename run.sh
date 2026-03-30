5d70be5f3b023b5d955a8e044a9717926f090107
Pinho <rafaelpinhotsilva@gmail.com>
2026-03-30 18:36:02 +0100

    Refactored run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,24 @@
 #!/bin/bash 
 
-mkdir -p code/cmake-build &&
-cd code/cmake-build &&
-cmake .. &&
-make &&
-cd ../.. &&
-./code/cmake-build/da_tool
+if [ "$#" -eq 0 ]; then
+    mkdir -p code/cmake-build &&
+    mkdir -p assignments &&
+    cd code/cmake-build &&
+    cmake .. &&
+    make &&
+    cd ../.. &&
+    ./code/cmake-build/da_tool
+elif [ "$#" -eq 3 ] && [ "$1" = "-b" ]; then
+    mkdir -p code/cmake-build &&
+    mkdir -p assignments &&
+    cd code/cmake-build &&
+    cmake .. &&
+    make &&
+    cd ../.. &&
+    ./code/cmake-build/da_tool -b "dataset/input/$2.csv" "assignments/$3.csv"
+else
+    echo "Usage:"
+    echo "  $0"
+    echo "  $0 -b <input_filename> <output_filename>"
+    exit 1
+fi
